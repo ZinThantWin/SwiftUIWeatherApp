0337f05e4b44541712e48f57aad0bd038d85d209
ZinThantWin <zinthantwin1997@gmail.com>
2024-06-10 15:27:54 +0630

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by ကင်ဇို on 10/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
